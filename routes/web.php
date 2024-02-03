26115079c6e0c369f4587e57acb6578edc65d318
unknown <akbridha@gmail.com>
2024-02-03 21:10:50 +0800

    menambahkan adminLTE dan membersihkan contenmengganti landing dari main.blade menjadi dashboard.blade..  komponennya sama. tetapi dasboard menggunakan @section('content')

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('layouts.main');
+    // return view('layouts.main');
+    return view('layouts.dashboard');
 });
 // Route::get('/pasien', [PasienController::class, 'index']);
 
