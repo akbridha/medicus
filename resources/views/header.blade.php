ea1248381a05084ac2b195a9d7c6f3be6af6b50d
unknown <akbridha@gmail.com>
2024-02-07 23:04:04 +0800

    membenahi dashboard dan mengganti tampilan menggunakan container-fluid

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Laravel App</title>
+    <title>Medika</title>
     <!-- Tautan ke Bootstrap CSS -->
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-s27UV2zso+G8KLeT1hQcq1gL8Fy8tO0oMf8tF5A51P5B9RLxGOdsR8vYwWdJVy7ehADZhJ/zBv/ZsRV05mj0Dg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="/">Sanitatis Notatio Medicus dr.Najmi</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/"><i class="bi bi-person-rolodex"></i> Medicus dr.Najmi</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -28,14 +30,17 @@
                     <a class="nav-link" href="/pasien">Pasien</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/pasien">-</a>
+                    <div class="card">
+                        <a class="nav-link" href="#"> {{ date('d-m-Y') }}</a>
+                    </div>
                 </li>
+
             </ul>
         </div>
     </div>
 </nav>
 
-<div class="container">
+<div class="container-fluid">
     @yield('content') <!-- Ini adalah tempat konten dari view akan ditempatkan -->
 </div>
 
--- resources/views/layouts/pasien/index.blade.php
+++ resources/views/layouts/pasien/index.blade.php
@@ -1,90 +1,88 @@
 @extends('header') <!-- Anda perlu mengganti 'layouts.app' sesuai dengan layout Anda -->
 
 @section('content')
-<div class="container">
-    <h1>Daftar Pasien</h1>
+    <div class="container-fluid">
+        <h1>Daftar Pasien</h1>
+        @if (session()->has('key'))
+            @if (session('key')== 'Berhasil')
+                <div class="alert alert-success">
+                    Berhasil Menambahkan Data
+                </div>
+            @else
+                <div class="alert alert-danger">
+                    Gagal
+                </div>
+                <div class="alert alert-danger">
+                    {{ session('key') }}
+                </div>
+            @endif
+            @php
+                session()->forget('key');
+            @endphp
+        @endif
+
+        <a href="{{route('pasien.create')}}" class="btn btn-info mb-4 float-right">Tambah</a>
+        <form method="GET" action="{{ route('cari') }}">
+            <input type="text" name="kata_kunci" placeholder="Cari...">
+            <button type="submit">Cari</button>
+        </form>
 
 
-@if (session()->has('key'))
+            {{-- pengecekan apabila data yang dikirim kosong --}}
+        @if($pasiens->isEmpty())
+            <div class="alert alert-warning" role="alert">
+                Data Pasien tidak ditemukan
+            </div>
+        @else
+            <div class="container-fluid ">
+
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>NBL</th>
+                                <th>Nama</th>
+                                <th>Alamat</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($pasiens as $pasien)
+                            <tr>
+                                <td style="width: 100px;">{{ $pasien->NBL }}</td>
+                                <td>{{ $pasien->Nama }}</td>
+                                <td>{{ $pasien->Alamat }}</td>
+                                <td style="width: 150px;">
+                                    {{-- tombol kunjungan baru         --}}
+                                        <form method="POST" action="{{ route('rm.create') }}">
+                                            @csrf
+                                            <input type="hidden" name="id_pasien" value={{ $pasien->id }}>
+                                            <button type="submit" class="btn btn-success mb-2">Kunjungan Baru</button>
+                                        </form>
+                                        {{-- tombol untuk ke halaman riwayat --}}
+                                        <a href="{{ route('rm.show', ['id' =>$pasien->id ]) }}" class="btn btn-primary mb-3">Riwayat</a>
+                                        {{-- tombol untuk ke halaman edit pasien --}}
+
+                                        <a href="{{ route('pasien.edit', ['id' =>$pasien->id ]) }}" class="btn btn-warning mb-3">Ubah Data</a>
+                                        {{-- <form method="POST" action="{{route('pasien.edit')}}">
+                                            @csrf
+                                            <input type="hidden" name="id_pasien" value={{ $pasien->id }}>
+                                            <button type="submit" class="btn btn-warning mb-2">Edit Pasien</button>
+                                        </form> --}}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{-- //untuk pagination halamannya --}}
+                    {{$pasiens ->links()}}
+                </div>
+            </div>
 
 
-@if (session('key')== 'Berhasil')
-
-<div class="alert alert-success">
-    Berhasil Menambahkan Data
-</div>
-@else
-<div class="alert alert-danger">
-    Gagal
-</div>
-<div class="alert alert-danger">
-    {{ session('key') }}
-</div>
-
-@endif
-
-@php
-        session()->forget('key');
-@endphp
-
-@endif
-
-    <a href="{{route('pasien.create')}}" class="btn btn-info mb-4 float-right">Tambah</a>
-    <form method="GET" action="{{ route('cari') }}">
-        <input type="text" name="kata_kunci" placeholder="Cari...">
-        <button type="submit">Cari</button>
-    </form>
 
 
-    {{-- pengecekan apabila data yang dikirim kosong --}}
-    @if($pasiens->isEmpty())
-        <div class="alert alert-warning" role="alert">
-            Data Pasien tidak ditemukan
-        </div>
-    @else
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>NBL</th>
-                        <th>Nama</th>
-                        <th>Alamat</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($pasiens as $pasien)
-                        <tr>
-                            <td style="width: 100px;">{{ $pasien->NBL }}</td>
-                            <td>{{ $pasien->Nama }}</td>
-                            <td>{{ $pasien->Alamat }}</td>
-                            <td style="width: 150px;">
-                                {{-- tombol kunjungan baru         --}}
-                                <form method="POST" action="{{ route('rm.create') }}">
-                                    @csrf
-                                    <input type="hidden" name="id_pasien" value={{ $pasien->id }}>
-                                    <button type="submit" class="btn btn-success mb-2">Kunjungan Baru</button>
-                                </form>
-                                {{-- tombol untuk ke halaman riwayat --}}
-                                <a href="{{ route('rm.show', ['id' =>$pasien->id ]) }}" class="btn btn-primary mb-3">Riwayat</a>
-                                {{-- tombol untuk ke halaman edit pasien --}}
-
-                                <a href="{{ route('pasien.edit', ['id' =>$pasien->id ]) }}" class="btn btn-warning mb-3">Ubah Data</a>
-                                {{-- <form method="POST" action="{{route('pasien.edit')}}">
-                                    @csrf
-                                    <input type="hidden" name="id_pasien" value={{ $pasien->id }}>
-                                    <button type="submit" class="btn btn-warning mb-2">Edit Pasien</button>
-                                </form> --}}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            {{-- //untuk pagination halamannya --}}
-            {{$pasiens ->links()}}
-        </div>
-
-    @endif
-    {{-- akhir pengecekan apabila data yang dikirim kosong --}}
-</div>
+        @endif
+            {{-- akhir pengecekan apabila data yang dikirim kosong --}}
+    </div>
 @endsection
